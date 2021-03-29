2f8abf3a8e1aa163dc4dbdc6fe28998247698cfc
kalyan <kalyankk8790@gmail.com>
2021-03-29 16:19:58 +0530

    my first program in c

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+int a,b,c,z;
+printf("enter the values of a,b,c for addition");
+scanf("%d","%d","%d",&a,&b,&c);
+if (a=0||b=0||c=0){
+printf("enter values in natural numbers");
+}
+else{
+Z=a+b+c;
+print("value of addition is", z);
+}
+}
